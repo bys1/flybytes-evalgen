d698032e58f874feec5abff61b87c2a2498379bd
Björn Schönrock <bjorn.schonrock@live.nl>
2023-05-24 18:36:55 +0200

    lalala

--- build.sh
+++
@@ -1,5 +0,0 @@
-javac -cp rascal.jar -d build src/main/java/*/*.java
-javac -cp rascal.jar -d build src/main/java/*/*/*.java
-javac -cp rascal.jar -d build src/main/java/*/*/*/*.java
-cd build
-jar -cf fbeg.jar *
