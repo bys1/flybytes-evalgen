3beaf6d92ec06ff181db50efd1c47cd0594a77a9
Björn Schönrock <bjorn.schonrock@live.nl>
2023-06-08 21:53:28 +0200

    push

--- src/main/java/fbeg/demo/func/Memory.java
+++ src/main/java/fbeg/demo/func/Memory.java
@@ -5,37 +5,37 @@ import io.usethesource.vallang.IValueFactory;
 import java.lang.management.ManagementFactory;
 
 public class Memory {
-	
-	private IValueFactory vf;
-	
-	public Memory(final IValueFactory vf) {
-		this.vf = vf;
-	}
-	
-	private static long getUsedMemory() {
-		return	ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed()
-			+ 	ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed();
-	}
-	
-	private static long gcCount() {
-		long sum = 0;
-		for (var bean : ManagementFactory.getGarbageCollectorMXBeans()) {
-			final long count = bean.getCollectionCount();
-			if (count != -1) sum += count;
-		}
-		return sum;
-	}
-	
-	private static long getReallyUsedMemory() {
-		final long count = gcCount();
-		System.gc();
-		while (gcCount() == count) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {}
-		}
-		return getUsedMemory();
-	}
+    
+    private IValueFactory vf;
+    
+    public Memory(final IValueFactory vf) {
+        this.vf = vf;
+    }
+    
+    private static long getUsedMemory() {
+        return	ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed()
+            + 	ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed();
+    }
+    
+    private static long gcCount() {
+        long sum = 0;
+        for (var bean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            final long count = bean.getCollectionCount();
+            if (count != -1) sum += count;
+        }
+        return sum;
+    }
+    
+    private static long getReallyUsedMemory() {
+        final long count = gcCount();
+        System.gc();
+        while (gcCount() == count) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+        }
+        return getUsedMemory();
+    }
 
     private static long getStabilizedMemory() {
         long m;
@@ -53,9 +53,9 @@ public class Memory {
     public static void printMemory() {
         System.out.println(getReallyUsedMemory());
     }
-	
-	public void printMemoryR() {
-		printMemory();
-	}
-	
+    
+    public void printMemoryR() {
+        printMemory();
+    }
+    
 }
